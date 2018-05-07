c988c57dd89a91dd1aa983c4c9a7337eb74b418c
Tai Cristiano <taicristiano@Tais-MacBook-Pro.local>
2018-05-07 23:33:03 +0700

    Update message validate of normal setting

--- app/Http/Requests/NormalSettingRequest.php
+++ app/Http/Requests/NormalSettingRequest.php
@@ -32,7 +32,7 @@ class NormalSettingRequest extends Request
             'duration' => 'required',
             'quantity' => 'required|integer|min:1',
         ];
-    }    
+    }
 
     /**
      * message validate
@@ -41,6 +41,24 @@ class NormalSettingRequest extends Request
     public function messages()
     {
         return [
+            'paypal_fee_rate.required' => Lang::get('validation.nomar-setting.the_paypal_fee_rate_field_is_required'),
+            'paypal_fee_rate.percent' => Lang::get('validation.nomar-setting.the_paypal_fee_rate_field_is_must_be_from_1_to_100'),
+
+            'paypal_fixed_fee.required' => Lang::get('validation.nomar-setting.the_paypal_fixed_fee_field_is_required'),
+            'paypal_fixed_fee.integer' => Lang::get('validation.nomar-setting.the_paypal_fixed_fee_field_must_be_an_integer'),
+            'paypal_fixed_fee.greate_than_or_equal_zero' => Lang::get('validation.nomar-setting.the_paypal_fixed_fee_field_must_be_grate_than_or_equal_zero'),
+
+            'ex_rate_diff.required' => Lang::get('validation.nomar-setting.the_ex_rate_diff_field_is_required'),
+            'ex_rate_diff.integer' => Lang::get('validation.nomar-setting.the_ex_rate_diff_field_must_be_an_integer'),
+
+            'gift_discount.required' => Lang::get('validation.nomar-setting.the_gift_discount_field_is_required'),
+            'gift_discount.integer' => Lang::get('validation.nomar-setting.the_gift_discount_field_must_be_an_integer'),
+            'gift_discount.min' => Lang::get('validation.nomar-setting.the_gift_discount_field_is_must_be_from_1_to_100'),
+            'gift_discount.max' => Lang::get('validation.nomar-setting.the_gift_discount_field_is_must_be_from_1_to_100'),
+
+            'quantity.required' => Lang::get('validation.nomar-setting.the_quantity_field_is_required'),
+            'quantity.integer' => Lang::get('validation.nomar-setting.the_quantity_field_must_be_an_integer'),
+            'quantity.min' => Lang::get('validation.nomar-setting.the_quantity_field_is_must_be_greate_than_zero'),
         ];
     }
 }
--- resources/views/admin/setting/normal.blade.php
+++ resources/views/admin/setting/normal.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 @section('content')
 <div class="content-wrapper">
-    @include('layouts.component.header-content', ['text' => __('view.upload_csv')])
+    @include('layouts.component.header-content', ['text' => __('side_bar.normal_setting')])
     <!-- Main content -->
     <section class="content">
         <!-- Default box -->
@@ -11,71 +11,84 @@
                     <div class="box-header with-border">
                         @if($isShowButtonGetToken)
                         <form action="{{ route('admin.user.api-get-session-id') }}" method="POST" role="form">
-                        @csrf
-                        <div class="form-group">
-                            <p>Ebayアクセストークン取得</p>
-                            <button class="btn btn-primary" type="submit"> Ebayに連携する</button>
-                        </div>
+                            @csrf
+                            <div class="form-group">
+                                <p>@lang('view.get_ebay_access_token')</p>
+                                <button class="btn btn-primary" type="submit">@lang('view.link_to_ebay')</button>
+                            </div>
                         </form>
                         @else
-                            <p>Ebayアクセストークン取得</p>
-                            <button class="btn btn-primary" type="button"> 連携済み</button>
+                        <p>@lang('view.get_ebay_access_token')</p>
+                        <button class="btn btn-primary" type="button">@lang('view.collaborated')</button>
                         @endif
                     </div>
                     <form action="{{ route('admin.user.normal_setting_post') }}" method="POST" role="form">
                         @csrf
-                    <div class="box-body">
-                        <div class="form-group">
-                            {!! Form::hidden('id', $setting->id) !!}
-                            <label for="store_id">出店形式 <span class="text-danger">(*)</span></label>
-                            {!! Form::select("store_id", $storeOption, old('store_id', isset($setting->store_id) ? $setting->store_id : ''), ['class' => 'form-control', 'id' => 'store_id']) !!}
-                            {!! $errors->first('store_id') ? '<p class="text-danger">'. $errors->first('store_id') .'</p>' : ''!!}
+                        <div class="box-body">
+                            <div class="form-group">
+                                {!! Form::hidden('id', $setting->id) !!}
+                                <label for="store_id">@lang('view.store_id') <span class="text-danger">(*)</span></label>
+                                {!! Form::select("store_id", $storeOption, old('store_id', isset($setting->store_id) ? $setting->store_id : ''), ['class' => 'form-control', 'id' => 'store_id']) !!}
+                                {!! $errors->first('store_id') ? '
+                                <p class="text-danger">'. $errors->first('store_id') .'</p>
+                                ' : ''!!}
+                            </div>
+                            <label for="paypal_fee_rate">@lang('view.paypal_fee_rate') <span class="text-danger">(*)</span></label>
+                            <div class="form-group input-group">
+                                {!! Form::text('paypal_fee_rate', old('paypal_fee_rate', isset($setting->paypal_fee_rate) ? $setting->paypal_fee_rate : ''), ['class' => 'form-control']) !!}
+                                <span class="input-group-addon">％</span>
+                            </div>
+                            {!! $errors->first('paypal_fee_rate') ? '
+                            <p class="text-danger">'. $errors->first('paypal_fee_rate') .'</p>
+                            ' : ''!!}
+                            <label for="ex_rate_diff">@lang('view.paypal_fixed_fee') <span class="text-danger">(*)</span></label>
+                            <div class="form-group input-group">
+                                {!! Form::text('paypal_fixed_fee', old('paypal_fixed_fee', isset($setting->paypal_fixed_fee) ? $setting->paypal_fixed_fee : ''), ['class' => 'form-control']) !!}
+                                <span class="input-group-addon">@lang('view.yen')</span>
+                            </div>
+                            {!! $errors->first('paypal_fixed_fee') ? '
+                            <p class="text-danger">'. $errors->first('paypal_fixed_fee') .'</p>
+                            ' : ''!!}
+                            <label for="ex_rate_diff">@lang('view.ex_rate_diff') <span class="text-danger">(*)</span></label>
+                            <div class="form-group input-group">
+                                {!! Form::text('ex_rate_diff', old('ex_rate_diff', isset($setting->ex_rate_diff) ? $setting->ex_rate_diff : ''), ['class' => 'form-control']) !!}
+                                <span class="input-group-addon">@lang('view.yen')</span>
+                            </div>
+                            {!! $errors->first('ex_rate_diff') ? '
+                            <p class="text-danger">'. $errors->first('ex_rate_diff') .'</p>
+                            ' : ''!!}
+                            <label for="gift_discount">@lang('view.gift_discount') <span class="text-danger">(*)</span></label>
+                            <div class="form-group input-group">
+                                {!! Form::text('gift_discount', old('gift_discount', isset($setting->gift_discount) ? $setting->gift_discount : ''), ['class' => 'form-control']) !!}
+                                <span class="input-group-addon">%</span>
+                            </div>
+                            {!! $errors->first('gift_discount') ? '
+                            <p class="text-danger">'. $errors->first('gift_discount') .'</p>
+                            ' : ''!!}
+                            <label for="duration">@lang('view.duration') <span class="text-danger">(*)</span></label>
+                            <div class="form-group input-group">
+                                {!! Form::select("duration", $durationOption, old('duration', isset($setting->duration) ? $setting->duration : ''), ['class' => 'form-control', 'id' => 'duration']) !!}
+                                <span class="input-group-addon">@lang('view.day')</span>
+                            </div>
+                            {!! $errors->first('duration') ? '
+                            <p class="text-danger">'. $errors->first('duration') .'</p>
+                            ' : ''!!}
+                            <div class="form-group">
+                                <label for="quantity">@lang('view.quantity') <span class="text-danger">(*)</span></label>
+                                {!! Form::text('quantity', old('quantity', isset($setting->quantity) ? $setting->quantity : ''), ['class' => 'form-control']) !!}
+                                {!! $errors->first('quantity') ? '
+                                <p class="text-danger">'. $errors->first('quantity') .'</p>
+                                ' : ''!!}
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-primary" type="button" id="btn-get-policy" {{ $isShowButtonGetToken ? 'disabled': ''}}>@lang('view.acquire_business_policy')</button>
+                            </div>
                         </div>
-                        <label for="paypal_fee_rate">割合 <span class="text-danger">(*)</span></label>
-                        <div class="form-group input-group">
-                            {!! Form::text('paypal_fee_rate', old('paypal_fee_rate', isset($setting->paypal_fee_rate) ? $setting->paypal_fee_rate : ''), ['class' => 'form-control']) !!}
-                            <span class="input-group-addon">％</span>
+                        <div class="box-footer">
+                            <div class="form-group text-center col-sm-4 col-sm-offset-4">
+                                <button class="btn btn-block btn-primary btn-lg" type="submit">@lang('view.save')</button>
+                            </div>
                         </div>
-                        {!! $errors->first('paypal_fee_rate') ? '<p class="text-danger">'. $errors->first('paypal_fee_rate') .'</p>' : ''!!}
-                        <label for="ex_rate_diff">固定値 <span class="text-danger">(*)</span></label>
-                        <div class="form-group input-group">
-                            {!! Form::text('paypal_fixed_fee', old('paypal_fixed_fee', isset($setting->paypal_fixed_fee) ? $setting->paypal_fixed_fee : ''), ['class' => 'form-control']) !!}
-                            <span class="input-group-addon">円</span>
-                        </div>
-                        {!! $errors->first('paypal_fixed_fee') ? '<p class="text-danger">'. $errors->first('paypal_fixed_fee') .'</p>' : ''!!}
-                        <label for="ex_rate_diff">為替調整 <span class="text-danger">(*)</span></label>
-                        <div class="form-group input-group">
-                            {!! Form::text('ex_rate_diff', old('ex_rate_diff', isset($setting->ex_rate_diff) ? $setting->ex_rate_diff : ''), ['class' => 'form-control']) !!}
-                            <span class="input-group-addon">円</span>
-                        </div>
-                        {!! $errors->first('ex_rate_diff') ? '<p class="text-danger">'. $errors->first('ex_rate_diff') .'</p>' : ''!!}
-                        <label for="gift_discount">ギフト件割引率 <span class="text-danger">(*)</span></label>
-                        <div class="form-group input-group">
-                            {!! Form::text('gift_discount', old('gift_discount', isset($setting->gift_discount) ? $setting->gift_discount : ''), ['class' => 'form-control']) !!}
-                            <span class="input-group-addon">%</span>
-                        </div>
-                        {!! $errors->first('gift_discount') ? '<p class="text-danger">'. $errors->first('gift_discount') .'</p>' : ''!!}
-                        <label for="duration">販売期間 <span class="text-danger">(*)</span></label>
-                        <div class="form-group input-group">
-                            {!! Form::select("duration", $durationOption, old('duration', isset($setting->duration) ? $setting->duration : ''), ['class' => 'form-control', 'id' => 'duration']) !!}
-                            <span class="input-group-addon">日</span>
-                        </div>
-                        {!! $errors->first('duration') ? '<p class="text-danger">'. $errors->first('duration') .'</p>' : ''!!}
-                        <div class="form-group">
-                            <label for="quantity">個数 <span class="text-danger">(*)</span></label>
-                            {!! Form::text('quantity', old('quantity', isset($setting->quantity) ? $setting->quantity : ''), ['class' => 'form-control']) !!}
-                            {!! $errors->first('quantity') ? '<p class="text-danger">'. $errors->first('quantity') .'</p>' : ''!!}
-                        </div>
-                        <div class="form-group">
-                            <button class="btn btn-primary" type="button" id="btn-get-policy" {{ $isShowButtonGetToken ? 'disabled': ''}}>ビジネスポリシー取得</button>
-                        </div>
-                        
-                    </div>
-                    <div class="box-footer">
-                        <div class="form-group text-center">
-                            <button class="btn btn-primary" type="submit">保存</button>
-                        </div>
-                    </div>
                     </form>
                     <form action="{{ route('admin.user.api-get-policy') }}" method="POST" role="form" id="form-get-policy">
                         @csrf
@@ -87,5 +100,5 @@
     @endsection
 </div>
 @section('script')
-    <script src="{{ asset('js/normal-setting.js') }}"></script>
+<script src="{{ asset('js/normal-setting.js') }}"></script>
 @endsection

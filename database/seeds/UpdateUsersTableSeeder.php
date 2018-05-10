72b86030ef53c6e71cf36e2ef885ee53e25c3c35
Tai Cristiano <taductaik56a3@gmail.com>
2018-05-10 21:26:12 +0700

    Update user seed

--- database/seeds/UpdateUsersTableSeeder.php
+++ database/seeds/UpdateUsersTableSeeder.php
@@ -16,33 +16,40 @@ class UpdateUsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = User::whereIn('type', [User::TYPE_SUPER_ADMIN, User::TYPE_GUEST_ADMIN])->get();
+        $users = User::get();
         foreach($users as $user) {
-            $setting = Setting::where('user_id', $user->id)->first();
-            if ($user->type == User::TYPE_GUEST_ADMIN) {
-                $authorization = Authorization::where('user_id', $user->id)->first();
-                if (!$authorization) {
-                    $dataInsert['user_id'] = $user->id;
-                    Authorization::insert($dataInsert);
-                }
-            }
-            if (!$setting) {
-                $dataSetting['user_id'] = $user->id;
-                $dataSetting['created_at'] = date('Y-m-d H:i:s');
-                $dataSetting['updated_at'] = date('Y-m-d H:i:s');
-                Setting::insert($dataSetting);
-                $this->shippingFee     = new ShippingFee;
-                $this->settingShipping = new SettingShipping;
-                // insert setting shipping and setting fee
-                $dataShipping = $this->settingShipping->getDataMaster($user->id);
-                foreach ($dataShipping as $key => $shipping) {
-                    $settingShippingId = $this->settingShipping->insertGetId($shipping);
-                    if ($shipping['shipping_name'] == 'EMS') {
-                        $dataShippingFee = $this->shippingFee->getDataMaster($settingShippingId, true);
-                    } else {
-                        $dataShippingFee = $this->shippingFee->getDataMaster($settingShippingId);
+            // if ($user->id == 2) {
+            //     dd(User::get());
+            // }
+            $user->user_code = User::generateUserCode();
+            $user->save();
+            if (in_array($user->type, [User::TYPE_SUPER_ADMIN, User::TYPE_GUEST_ADMIN])) {
+                $setting = Setting::where('user_id', $user->id)->first();
+                if ($user->type == User::TYPE_GUEST_ADMIN) {
+                    $authorization = Authorization::where('user_id', $user->id)->first();
+                    if (!$authorization) {
+                        $dataInsert['user_id'] = $user->id;
+                        Authorization::insert($dataInsert);
                     }
-                    $this->shippingFee->insert($dataShippingFee);
+                }
+                if (!$setting) {
+                    $dataSetting['user_id'] = $user->id;
+                    $dataSetting['created_at'] = date('Y-m-d H:i:s');
+                    $dataSetting['updated_at'] = date('Y-m-d H:i:s');
+                    Setting::insert($dataSetting);
+                    $this->shippingFee     = new ShippingFee;
+                    $this->settingShipping = new SettingShipping;
+                    // insert setting shipping and setting fee
+                    $dataShipping = $this->settingShipping->getDataMaster($user->id);
+                    foreach ($dataShipping as $key => $shipping) {
+                        $settingShippingId = $this->settingShipping->insertGetId($shipping);
+                        if ($shipping['shipping_name'] == 'EMS') {
+                            $dataShippingFee = $this->shippingFee->getDataMaster($settingShippingId, true);
+                        } else {
+                            $dataShippingFee = $this->shippingFee->getDataMaster($settingShippingId);
+                        }
+                        $this->shippingFee->insert($dataShippingFee);
+                    }
                 }
             }
         }
